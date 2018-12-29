daa5fb0e057ed4adcb30529bb9a213d1944ab7d0
cmmccoy1996@gmail.com <cmmccoy2@asu.edu>
2018-12-29 14:11:27 -0700

    need to push to the database then redirect then get the data from the data base.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT=3000;
 
 //mongoose.connect(process.env.MONGODB_URI.toString());
 
-var key="AIzaSyCJyl_DjWAyQrgaRq_xAQjhPb22zUoi_xw";
+var _key="AIzaSyCJyl_DjWAyQrgaRq_xAQjhPb22zUoi_xw";
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -23,75 +23,29 @@ app.get('/',function (req,res) {
   res.render('main.ejs');
 });
 
-/*app.post('/main',function(req,res){
-	var address=req.body.address;
-	var link=req.body.link;
-	var locations=getObject(address,link,key);
-	res.redirect("/map");
-});*/
-
-app.get("/map",function(req,res){
-	res.render("map.ejs");
-});
 
 app.listen(PORT,function() {
     console.log("Listening on Port:"+PORT);
 });
 
-/*function getLatLong(address,key)
-{
-	var url = "https://maps.googleapis.com"+
-			"/maps/api/geocode/json?address="+address+"&key="+key;
-	//console.log(url);
-	var location= {lat:0,lng:0};
-
-	request({
-	    url: url,
-	    json: true
-	}, function (error, response, body) {
-	    if (!error && response.statusCode === 200) {
-	        location = body.results[0].geometry.location// Print the json response
-	        return location;
-	    }
-	});
-}
-function getObject(address,link,key)
-{
-	var object=[];
-	for(var i=0;i<address.length;++i)
-	{
-		if(address[i])
-		{
-			var position=getLatLong(address[i],key);
-			console.log(position);
-
-			var cur={
-				position:position,
-				address:address[i],
-				link:link[i]};
-			object.push(cur);
-		}
-	}
-	console.log(object[0].position);
-	return object;
-}*/
 app.post('/main',function(req,res){
+
 	var address=req.body.address;
 	var link=req.body.link;
 	var objects=[];
 	var i=0;
+
 	async.forEachOf(address,(value,key,callback)=>{
 		console.log("inside");
 
 		var url = "https://maps.googleapis.com"+
-			"/maps/api/geocode/json?address="+address+"&key="+key;
-	//console.log(url);
+			"/maps/api/geocode/json?address="+address+"&key="+_key;
+		console.log(url);
 		var location= {lat:0,lng:0};
 
 		request({url: url,json: true}, function (error, response, body) {
 		    if (!error && response.statusCode === 200) {
-		       // location = body.results[0].geometry.location// Print the json response
-		       location="inside";
+		       location=body;
 		       objects.push(location);
 		    }
 		    else if(error)
@@ -99,21 +53,20 @@ app.post('/main',function(req,res){
 		    	console.log(error);
 		    }
 		    if(i==address.length-1)
-		{
-			console.log(objects);
-			callback(objects);
-		}
-		i++;
+			{
+				console.log(objects);
+				callback(objects);
+			}
+			i++;
 		});
-
 
 	},function(objects){
 		console.log(objects.length);
-		res.send(objects);
+		res.redirect("/map");
+		app.get("/map",function(req,res){
+			console.log(location);
+			res.render("map.ejs",{location:objects});
+		});
 	});
-
-
-
-
 
 });
