1b45a0c530873dd560ac0de2d932238bb2de661c
cmmccoy1996@gmail.com <cmmccoy2@asu.edu>
2018-12-29 17:29:29 -0700

    finally...

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/main',function(req,res){
 	var objects=[];
 	var i=0;
 
-	async.forEachOf(address,(value,key,callback)=>{
+	/*async.forEachOf(address,(value,key,callback)=>{
 		console.log("inside");
 
 		var url = "https://maps.googleapis.com"+
@@ -67,6 +67,43 @@ app.post('/main',function(req,res){
 			console.log(location);
 			res.render("map.ejs",{location:objects});
 		});
+	});*/
+
+	/*var itemsProcessed = 0;
+
+	[1, 2, 3].forEach((item, index, array) => {
+		asyncFunction(item, () => {
+			itemsProcessed++;
+			if(itemsProcessed === array.length) {
+			  callback();
+			}
+		});
+	});*/
+	var url=[];
+	for(i=0;i<address.length;++i)
+	{
+		var urll = "https://maps.googleapis.com"+
+			"/maps/api/geocode/json?address="+address[i]+"&key="+_key;
+		url.push(urll);
+	}
+	var itemsProcessed = 0;
+	url.forEach(function(item,i,array){
+		request({url: item,json: true}, function (error, response, body) {
+		    if (!error && response.statusCode === 200) {
+		       location=body.results[0].geometry.location;
+		       objects.push({location:location,address:address[i]});;
+		    }
+		    itemsProcessed++;
+		    if(itemsProcessed === array.length)
+		    {
+		    	callback(objects);
+		    }
+		});
 	});
+	function callback (objects) { 
+		console.log(objects);
+		 }
+		
+
 
 });
