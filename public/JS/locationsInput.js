2ed8d8ac27e54d21c12570c2105592a1c62adaac
cmmccoy1996@gmail.com <cmmccoy2@asu.edu>
2019-01-08 19:51:24 -0700

    made content and design need to figure out how to animate it

--- public/JS/locationsInput.js
+++ public/JS/locationsInput.js
@@ -1,11 +1,85 @@
 window.onload = function() {
 	remove();
 	add();
+	contentOrDesign();
 
 
 }
+/*function fadeInDesign()
+{
+	var designSide=document.getElementById("design");
+	designSide.classList.add('fadeInLeft');
+	var contentSide=document.getElementById("location");
+
+	contentSide.classList.add('fadeInLeft');
+
+	if(contentSide.classList.contains('fadeInRight'))
+	{
+		contentSide.classList.remove('fadeInRight');
+	}
+	//contentSide.classList.add("probootstrap-animated");
+}
+function fadeInContent()
+{
+	var designSide=document.getElementById("design");
+	var contentSide=document.getElementById("location");
+	contentSide.classList.add('fadeInRight');
+
+	contentSide.classList.add('fadeInLeft');
+
+	if(designSide.classList.contains('fadeInLeft'))
+	{
+		designSide.classList.remove('fadeInLeft');
+	}
+	//designSide.classList.add("probootstrap-animated");
+}*/
+function contentOrDesign(){
+	var content=document.getElementById('contentBtn');
+	var design=document.getElementById('designBtn');
+
+//click content
+content.addEventListener("click",function(){
 
 
+	//fadeInContent();
+	var designSide=document.getElementById("design");
+	var contentSide=document.getElementById("location");
+
+	contentSide.style.display="block";
+	designSide.style.display="none";
+
+	if(this.classList.contains('selected')==false)
+	{
+
+		this.classList.add('selected');
+		var design=document.getElementById('designBtn');
+		design.classList.remove('selected');
+
+	}
+
+});
+
+//click design
+design.addEventListener("click",function(){
+
+	//fadeInDesign();
+	var designSide=document.getElementById("design");
+	var contentSide=document.getElementById("location");
+
+	contentSide.style.display="none";
+	designSide.style.display="block";
+
+	if(this.classList.contains('selected')==false)
+	{
+		this.classList.add('selected');
+		this.classList.add('fadeInRight');
+		var content=document.getElementById('contentBtn');
+		content.classList.remove('selected');
+
+	}
+});
+
+}
 
 function remove(){
 	var removeBtn=document.querySelectorAll(".my-0");
@@ -13,14 +87,14 @@ function remove(){
 	for(var i=0;i<removeBtn.length;++i)
 	{
 		removeBtn[i].addEventListener("click",function(){
-				var lenBtn=document.querySelectorAll(".my-0").length;
-				console.log(lenBtn);
+			var lenBtn=document.querySelectorAll(".my-0").length;
+			console.log(lenBtn);
 
-				if(lenBtn!=1)
-				{
-					this.parentElement.parentElement.parentElement.remove();
-				}
-			
+			if(lenBtn!=1)
+			{
+				this.parentElement.parentElement.parentElement.remove();
+			}
+
 		});
 	}
 }
@@ -40,14 +114,14 @@ function add(){
 
 			p.appendChild(textNode);
 			endOfTable.insertAdjacentHTML( 'afterend',"<tr class='data'>"+
-							"<td class='pt-3-half'><input type='text' name='address'></td>"+
-							"<td class='pt-3-half'><input type='text' name='city'></td>"+
-							"<td class='pt-3-half'><input type='text' name='state'></td>"+
-							"<td class='pt-3-half'><input type='text' name='companyName'></td>"+
-							"<td class='pt-3-half'><input type='text' name='phone'></td>"+
-							"<td class='pt-3-half'><input type='text' name='link'></td>"+
-							"<td><span class='table-remove'><button type='button' class='btn btn-danger btn-rounded btn-sm my-0'>Delete</button></span>"+
-							"</td></tr>");
+				"<td class='pt-3-half'><input type='text' name='address'></td>"+
+				"<td class='pt-3-half'><input type='text' name='city'></td>"+
+				"<td class='pt-3-half'><input type='text' name='state'></td>"+
+				"<td class='pt-3-half'><input type='text' name='companyName'></td>"+
+				"<td class='pt-3-half'><input type='text' name='phone'></td>"+
+				"<td class='pt-3-half'><input type='text' name='link'></td>"+
+				"<td><span class='table-remove'><button type='button' class='btn btn-danger btn-rounded btn-sm my-0'>Delete</button></span>"+
+				"</td></tr>");
 
 			remove();
 
--- public/JS/main.js
+++ public/JS/main.js
@@ -1,131 +1,395 @@
-<script>
-	var clientPositions=<%-JSON.stringify(position)%>;
+(function() {
+	'use strict';
 
-function initMap() {
-	var infoWindow = new google.maps.InfoWindow();
+	/*----------------------------------------
+		Detect Mobile
+	----------------------------------------*/
+	var isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android/i);
+		},
+			BlackBerry: function() {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+			iOS: function() {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+			Opera: function() {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+			Windows: function() {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+			any: function() {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
 
-	map = new google.maps.Map(document.getElementById('map'), {
-		center:clientPositions[0].position,
-		zoom: 4
+	/*----------------------------------------
+		Back to top
+	----------------------------------------*/
+	var backToTop = function() {
+		$('.js-backtotop').on('click', function(e){
+			e.preventDefault();
+			$('html, body').animate({
+	      scrollTop: $('body').offset().top
+	    }, 700, 'easeInOutExpo');
+		});
+	}
+
+	/*----------------------------------------
+		Burger Menu
+	----------------------------------------*/	
+	var mobileMenuControl = function() {
+		
+
+		// click burger menu
+		$('.probootstrap-burger-menu').on('click', function(e){
+
+			if ($('body').hasClass('show')) {
+				$('.probootstrap-burger-menu').removeClass('active');
+				$('body').removeClass('show');
+			} else {
+				$('.probootstrap-burger-menu').addClass('active');
+				$('body').addClass('show');
+			}
+			
+			e.preventDefault();
+
+		});
+
+		if ($(window).width() > 766) {
+			console.log('resizing');
+			$('body').removeClass('probootstrap-mobile-menu-active');
+			$('.probootstrap-burger-menu').removeClass('active');
+		} else {
+			$('body').addClass('probootstrap-mobile-menu-active');
+		}
+
+		$(window).resize(function(){
+			console.log($(window).width());
+			if ($(window).width() > 766) {
+				console.log('resizing');
+				$('body').removeClass('probootstrap-mobile-menu-active');
+				$('.probootstrap-burger-menu').removeClass('active');
+			} else {
+				$('body').addClass('probootstrap-mobile-menu-active');
+			}
+		});
+
+		// Click outside of the Mobile Menu content
+		$(document).click(function (e) {
+	    var container = $(".probootstrap-nav, .probootstrap-burger-menu");
+	    
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('body').hasClass('show') ) {
+					$('body').removeClass('show');
+					$('.probootstrap-burger-menu').removeClass('active');
+				}
+	    }
+		});
+		
+
+	};
+
+	/*----------------------------------------
+		Search
+	----------------------------------------*/
+	var searchControl = function() {
+		$('.js-probootstrap-search').on('click', function(){
+			$('#probootstrap-search').addClass('active');
+			setTimeout(function(){
+				$('#probootstrap-search').find('#search').focus().select();
+			}, 500);
+		});
+		$('.js-probootstrap-close').on('click', function(){
+			$('#probootstrap-search').removeClass('active');
+		});
+	};
+
+	/*----------------------------------------
+		Menu Hover
+	----------------------------------------*/
+	var menuHover = function() {
+		if (!isMobile.any()) {
+			$('.probootstrap-navbar .navbar-nav li.dropdown').hover(function() {
+			  $(this).find('> .dropdown-menu').stop(true, true).delay(200).fadeIn(500).addClass('animated-fast fadeInUp');
+			}, function() {
+				$(this).find('> .dropdown-menu').stop(true, true).fadeOut(200).removeClass('animated-fast fadeInUp')
+			});
+		}
+	}
+	/*----------------------------------------
+		Carousel
+	----------------------------------------*/
+	var owlCarousel = function(){
+
+		var owl = $('.owl-carousel-carousel');
+		owl.owlCarousel({
+			items: 3,
+			loop: true,
+			margin: 20,
+			nav: false,
+			dots: true,
+			smartSpeed: 800,
+			autoHeight: true,
+			navText: [
+		      "<i class='icon-keyboard_arrow_left owl-direction'></i>",
+		      "<i class='icon-keyboard_arrow_right owl-direction'></i>"
+	     	],
+	     	responsive:{
+	        0:{
+	            items:1
+	        },
+	        400:{
+	            items:1
+	        },
+	        600:{
+	            items:2
+	        },
+	        1000:{
+	            items:3
+	        }
+	    	}
+		});
+
+		var owl = $('.owl-carousel-fullwidth');
+		owl.owlCarousel({
+			items: 1,
+			loop: true,
+			margin: 20,
+			nav: false,
+			dots: true,
+			smartSpeed: 800,
+			autoHeight: true,
+			autoplay: true,
+			navText: [
+		      "<i class='icon-keyboard_arrow_left owl-direction'></i>",
+		      "<i class='icon-keyboard_arrow_right owl-direction'></i>"
+	     	]
+		});
+
+		var owl = $('.owl-work');
+		owl.owlCarousel({
+			stagePadding: 150,
+			loop: true,
+			margin: 20,
+			nav: true,
+			dots: false,
+			mouseDrag: false,
+			autoWidth: true,
+			autoHeight: true,
+	    autoplay: true,
+	    autoplayTimeout:2000,
+	    autoplayHoverPause:true,
+			navText: [	
+				"<i class='icon-chevron-thin-left'></i>",
+				"<i class='icon-chevron-thin-right'></i>"
+			],
+			responsive:{
+			  0:{
+		      items:1,
+		      stagePadding: 10
+			  },
+			  500:{
+			  	items:2,
+		      stagePadding: 20
+			  },
+			  600:{
+		      items:2,
+		      stagePadding: 40
+			  },
+			  800: {
+			  	items:2,
+			  	stagePadding: 100
+			  },
+			  1100:{
+		      items:3
+			  },
+			  1400:{
+		      items:4
+			  },
+			}
+		});
+	};
+
+	
+	/*----------------------------------------
+		Slider
+	----------------------------------------*/
+	var flexSlider = function() {
+	  $('.flexslider').flexslider({
+	    animation: "fade",
+	    prevText: "",
+	    nextText: "",
+	    slideshow: true
+	  });
+	}
+
+	
+	/*----------------------------------------
+		Content Animation
+	----------------------------------------*/
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.probootstrap-animate').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('probootstrap-animated') ) {
+				
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .probootstrap-animate.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							var effect = el.data('animate-effect');
+							if ( effect === 'fadeIn') {
+								el.addClass('fadeIn probootstrap-animated');
+							} else if ( effect === 'fadeInLeft') {
+								el.addClass('fadeInLeft probootstrap-animated');
+							} else if ( effect === 'fadeInRight') {
+								el.addClass('fadeInRight probootstrap-animated');
+							} else {
+								el.addClass('fadeInUp probootstrap-animated');
+							}
+							el.removeClass('item-animate');
+						},  k * 200, 'easeInOutExpo' );
+					});
+					
+				}, 100);
+				
+			}
+
+		} , { offset: '95%' } );
+	};
+
+	
+
+	/*----------------------------------------
+		Counter Animation
+	----------------------------------------*/
+	var counter = function() {
+		$('.js-counter').countTo({
+			 formatter: function (value, options) {
+	      return value.toFixed(options.decimals);
+	    },
+		});
+	};
+	var counterWayPoint = function() {
+		if ($('#probootstrap-counter').length > 0 ) {
+			$('#probootstrap-counter').waypoint( function( direction ) {
+										
+				if( direction === 'down' && !$(this.element).hasClass('probootstrap-animated') ) {
+					setTimeout( counter , 400);					
+					$(this.element).addClass('probootstrap-animated');
+				}
+			} , { offset: '90%' } );
+		}
+	};
+
+	var magnificPopupControl = function() {
+
+
+		$('.image-popup').magnificPopup({
+			type: 'image',
+			removalDelay: 300,
+			mainClass: 'mfp-with-zoom',
+			gallery:{
+				enabled:true
+			},
+			zoom: {
+				enabled: true, // By default it's false, so don't forget to enable it
+
+				duration: 300, // duration of the effect, in milliseconds
+				easing: 'ease-in-out', // CSS transition easing function
+
+				// The "opener" function should return the element from which popup will be zoomed in
+				// and to which popup will be scaled down
+				// By defailt it looks for an image tag:
+				opener: function(openerElement) {
+				// openerElement is the element on which popup was initialized, in this case its <a> tag
+				// you don't need to add "opener" option if this code matches your needs, it's defailt one.
+				return openerElement.is('img') ? openerElement : openerElement.find('img');
+				}
+			}
+		});
+
+		$('.with-caption').magnificPopup({
+			type: 'image',
+			closeOnContentClick: true,
+			closeBtnInside: false,
+			mainClass: 'mfp-with-zoom mfp-img-mobile',
+			image: {
+				verticalFit: true,
+				titleSrc: function(item) {
+					return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+				}
+			},
+			zoom: {
+				enabled: true
+			}
+		});
+
+
+		$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+
+      fixedContentPos: false
+    });
+	}
+
+	/*----------------------------------------
+		Stellar
+	----------------------------------------*/
+	var stellarInit = function() {
+		if( !isMobile.any() ) {
+			$(window).stellar();
+		}
+	};
+
+
+	var progressBarControl = function() {
+		if ( $(".progress-bar-s2").length > 0 ) {
+      var $progress_bar = $('.progress-bar-s2');
+
+      $progress_bar.appear();
+      $(document.body).on('appear', '.progress-bar-s2', function() {
+          var current_item = $(this);
+          if (!current_item.hasClass('appeared')) {
+              var percent = current_item.data('percent');
+              current_item.append('<span>' + percent + '%' + '</span>').css('width', percent + '%').addClass('appeared');
+          }
+          
+      });
+    };
+	}
+
+	/*----------------------------------------
+		Document Ready 
+	----------------------------------------*/
+	$(document).ready(function(){
+		menuHover();
+		counterWayPoint();
+		contentWayPoint();
+		backToTop();
+		searchControl();
+		magnificPopupControl();
+		stellarInit();
+		progressBarControl();
+		mobileMenuControl();
 	});
 
-	var marker=[];
+	$(window).load(function(){
+		owlCarousel();
+		flexSlider();
+	});
 
-	for(var i=0;i<clientPositions.length;++i)
-	{
-		marker = new google.maps.Marker({
-			position:clientPositions[i].position,
-	        map:map,
-	        title:clientPositions[i].name,
-            address:clientPositions[i].address
-		});
-		 google.maps.event.addListener(marker, 'click', function(){
-                infoWindow.setContent("<div class='custom-padding-5'><h4>"+this.title+"</h4>"+
-                                      "<p>"+this.address+"</p>"+
-                                      "<button value='2' class='main-color btn' onclick='getDirections("+'"'+this.address+'"'+")'>Directions</button></div>");
-                infoWindow.open(map, this);
-            
-         });
+	
 
-	}
-	console.log("marker:")
-	console.log(marker);
-	displayStores(clientPositions);
-
-
-	var input = document.getElementById('filter_users');
-    var autocomplete = new google.maps.places.Autocomplete(input);
-
-	var searchBox = new google.maps.places.SearchBox(input);
-
-         map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
-        });
-
-        searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
-
-          if (places.length == 0) {
-            return;
-          }
-          var bounds = new google.maps.LatLngBounds();
-          places.forEach(function(place) {
-            if (!place.geometry) {
-              console.log("Returned place contains no geometry");
-              return;
-            }
-            if (place.geometry.viewport) {
-              // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
-            } else {
-              bounds.extend(place.geometry.location);
-            }
-          });
-          map.fitBounds(bounds);
-        });
-
-}
-
-
-function getDirections(address)
-{
-    window.location.href = "https://www.google.com/maps/search/"+address;
-}
-
-function displayStores(myMarkers)
-{
-	console.log("executed"+myMarkers.length);
-    var storelocations=document.getElementById("store-locations");
-    for(var i=0;i<myMarkers.length;++i)
-    {
-        var li=document.createElement("li");
-
-        var node=document.createElement("div");
-        node.classList.add("border-top-bottom");
-        node.classList.add("custom-padding");
-        node.classList.add("store-li");
-        node.value=i;
-
-        var title=document.createElement("p");
-        title.classList.add("title");
-        textTitle=document.createTextNode(myMarkers[i].name);
-        title.appendChild(textTitle);
-
-        node.appendChild(title);
-
-        var address=document.createElement("p");
-        var addressText=document.createTextNode(myMarkers[i].address);
-        address.appendChild(addressText);
-
-        node.appendChild(address);
-
-
-        var direction=document.createElement("button");
-        var directionText=document.createTextNode("Directions");
-        direction.appendChild(directionText);
-        direction.setAttribute("onclick",'getDirections('+'"'+myMarkers[i].address+'"'+')');
-        direction.classList.add('main-color');
-        direction.classList.add('btn');
-
-        node.appendChild(direction);
-
-        li.appendChild(node);
-
-        li.style="list-style-type: none;";
-
-
-        storelocations.appendChild(li);
-    }
-    var storeUL=document.querySelectorAll(".store-li");
-
-
-    for(var i=0;i<myMarkers.length;++i)
-    {
-        storeUL[i].addEventListener("click",function(){
-            map.setCenter(myMarkers[this.value].position);
-        });
-    }
-}
-
-</script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCJyl_DjWAyQrgaRq_xAQjhPb22zUoi_xw&libraries=places&callback=initMap" async defer></script>
+})();
