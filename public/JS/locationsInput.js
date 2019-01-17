18dfa71c2903bd5443ca90d2fdddef88ec34da73
cmmccoy1996@gmail.com <cmmccoy2@asu.edu>
2019-01-17 15:35:10 -0700

    request from clientSide works!!!

--- public/JS/locationsInput.js
+++ public/JS/locationsInput.js
@@ -1,4 +1,4 @@
-	function Location(title,address,link,phoneNumber,place,position){
+function Location(title,address,link,phoneNumber,place,position){
 		this.title=title;
 		this.address=address;
 		this.link=link;
@@ -6,22 +6,33 @@
 		this.placeId=place;
 		this.position=position;
 };
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
 var locations=[];
-var location1=new Location("Loation 1",null,null,null,0,{lat:34.132853,lng:-118.5316293});
+var location1=new Location("Location 1","","","",0,{lat:34.132853,lng:-118.5316293});
 locations.push(location1);
-var location1=new Location("Loation 2",null,null,null,1,{lat:34.132853,lng:-117.5316293});
+var location1=new Location("Location 2","","","",1,{lat:34.132853,lng:-117.5316293});
 locations.push(location1);
-var location1=new Location("Loation 3",null,null,null,2,{lat:35.132853,lng:-118.5316293});
+var location1=new Location("Location 3","","","",2,{lat:35.132853,lng:-118.5316293});
 locations.push(location1);
-var location1=new Location("Loation 4",null,null,null,3,{lat:34.232853,lng:-118.7316293});
+var location1=new Location("Location 4","","","",3,{lat:34.232853,lng:-118.7316293});
 locations.push(location1);
-var location1=new Location("Loation 5",null,null,null,4,{lat:34.4553,lng:-117.6316293});
+var location1=new Location("Location 5","","","",4,{lat:34.4553,lng:-117.6316293});
 locations.push(location1);
-//loadLocations->addInputs->add->delete->loadLocations->addInputs->add->delete->loadLocations
-//contentOrDesign
 window.onload = function() {
 	loadLocations();
 	addLocation();
+	contentOrDesign();
 }
 function loadLocations()
 {
@@ -59,6 +70,7 @@ function loadLocations()
 	}
 	var btnContainer=document.querySelectorAll('.btnContainer');
 	btnContainer[btnContainer.length-1].scrollIntoView();
+	addInputs();
 	deleteBtn();
 	initMap();
 }
@@ -138,7 +150,7 @@ function deleteBtn()
 		});
 	}
 }
-/*function addInputs(callback)
+function addInputs()
 {
 	var locationBtn=document.querySelectorAll(".clickable");
 	console.log(locationBtn.length);
@@ -155,17 +167,19 @@ function deleteBtn()
 
 			//fill in the inputs
 			var titleInput=document.getElementsByName("title")[0];
-			var placeIdInput=document.getElementsByName("place")[0];
+			var placeIdInput=document.getElementsByName("placeId")[0];
 			titleInput.value=locations[this.id].title;
 			placeIdInput.value=locations[this.id].placeId;
+			var addressInput=document.getElementsByName("address")[0];
+			addressInput.value=locations[this.id].address;
+
+
+			submitLocation();
 
 
 		});
 	}
-	callback(addLocation(function(){
-		console.log("started addLocation");
-	}));
-}*/
+}
 function updateArray(array,deleteIndex)
 {
 	var newArray=[];
@@ -226,185 +240,49 @@ design.addEventListener("click",function(){
 	}
 });
 
-}
-/*function Location(title,address,link,phoneNumber){
-		this.title=title;
-		this.address=address;
-		this.link=link,
-		this.phoneNumber=phoneNumber;
-
-		this.getAddress=function(){
-			return this.address;
-		}
-
-};
-
-var locations=[];
-var location1=new Location("location 1",null,null,null);
-locations.push(location1);
-var location1=new Location("location 2",null,null,null);
-locations.push(location1);
-
-
-
-window.onload = function() {
-	loadLocations();
-	addInputs();
-	deleteBtn();
-	add();
-	contentOrDesign();
-}
-
-function deleteBtn()
-{
-	var trashBtn=document.querySelectorAll('.myTrash');
-	for(var i=0;i<trashBtn.length;++i)
-	{
-		trashBtn[i].addEventListener('click',function(){
-			var id=this.parentElement.id;
-			var form=document.getElementById(id);
-			form.submit();
-		});
-	}
-
-}
-function loadLocations()
-{
-	var scrollDiv=document.querySelectorAll(".scrollDiv")[0];
-
-
-	for(var i=0;i<locations.length;++i)
-	{
-
-		scrollDiv.insertAdjacentHTML('beforeend',``);
-
-	}
-}
-function contentOrDesign(){
-	var content=document.getElementById('contentBtn');
-	var design=document.getElementById('designBtn');
-
-//click content
-content.addEventListener("click",function(){
-
-
-	//fadeInContent();
+var backBtn=document.getElementById("backToLocation");
+backBtn.addEventListener("click",function(){
+	
 	var designSide=document.getElementById("design");
 	var contentSide=document.getElementById("location");
-	var editLocation=document.getElementById("editLocation");
 
 	contentSide.style.display="block";
 	designSide.style.display="none";
 	editLocation.style.display="none";
-
-	if(this.classList.contains('selected')==false)
-	{
-
-		this.classList.add('selected');
-		var design=document.getElementById('designBtn');
-		design.classList.remove('selected');
-
-	}
-
-});
-
-//click design
-design.addEventListener("click",function(){
-
-	var designSide=document.getElementById("design");
-	var contentSide=document.getElementById("location");
-
-	contentSide.style.display="none";
-	designSide.style.display="block";
-	editLocation.style.display="none";
-
-	if(this.classList.contains('selected')==false)
-	{
-		this.classList.add('selected');
-		this.classList.add('fadeInRight');
-		var content=document.getElementById('contentBtn');
-		content.classList.remove('selected');
-
-	}
 });
 
 }
+function submitLocation()
+{
+	var submitBtn=document.getElementById("submitLocation");
+	submitBtn.addEventListener("click",function(){
+		var titleInput=document.getElementsByName("title")[0];
+		var placeIdInput=document.getElementsByName("placeId")[0];
+		locations[placeIdInput.value].title=titleInput.value;
+		var addressInput=document.getElementsByName("address")[0];
+		locations[placeIdInput.value].address=addressInput.value;
+		if(addressInput!=locations[placeIdInput.value].address)
+		{
+			var client = new HttpClient();
+			var urll = "https://maps.googleapis.com/maps/api/geocode/json?address="+locations[placeIdInput.value].address+"&key="+"AIzaSyCJyl_DjWAyQrgaRq_xAQjhPb22zUoi_xw";
+			client.get(urll, function(response) {
+				response=JSON.parse(response);
+				console.log(response);
+				console.log(response.status);
+				if(response.status=="OK")
+				{
+					var location=response.results[0].geometry.location;
+					locations[placeIdInput.value].position=location;
+					initMap();
+				}
 
-function add(){
-	var addBtn=document.querySelectorAll(".addLocationBtn")[0];
-
-
-	addBtn.addEventListener("click",function(){
-	var table=document.querySelectorAll(".btnContainer");
-	var endOfTable=table[table.length-1];
-	var textNode="Location "+(table.length+1);
-	var newLocation=new Location(textNode,null,null,null,(table.length+1));
-
-	locations.push(newLocation);
-
-	endOfTable.insertAdjacentHTML( 'afterend',`<div class='col-lg-12 padding-left-30 padding-right-30 btnContainer padding-top-20'>
-						<div class='col-lg-1 clickable' >
-							<i class='fas fa-map-marker-alt'></i>
-						</div>
-						<div class='col-lg-6 clickable'>
-							<p style='margin-bottom: 0px;'>location `+(table.length+1)+`</p>
-						</div>
-						<div class='col-lg-1'>
-							<form id='deleteForm`+table.length+`' method='post' action='/delete'>
-								<i class='myTrash fas fa-trash-alt'></i>
-								<input type='text' name='locationId' style='display:none' value='`+locations[table.length].title+`'>
-							</form>
-						</div>
-						<div class='col-lg-4 clickable'>
-							<i style='float:right' class='fas fa-arrow-circle-right '></i>
-						</div>
-					</div>`);
-
-
-	endOfTable.scrollIntoView();
-	deleteBtn();
-	addInputs();
-
+				
+			});
+		}
+		initMap();
+		loadLocations();
 
 	});
-	
 }
 
-function addInputs()
-{
-	var locationBtn=document.querySelectorAll(".clickable");
-	var index=locationBtn.length/3;
-	console.log(locationBtn.length);
-	var trashBtn=document.querySelectorAll(".myTrash");
-	for(let i=0;i<locationBtn.length;++i)
-	{
-		let button=locationBtn[i];
-		button.addEventListener("click",function(){
-			var positionSlide=document.getElementById("location");
-			positionSlide.style.display="none";
-			var editLocationSlide=document.getElementById("editLocation");
-			editLocationSlide.style.display="block";
-
-			//fill in the inputs
-			var titleInput=document.getElementsByName("title")[0];
-			var placeIdInput=document.getElementsByName("place")[0];
-			//IMPORTANT NEED TO FILL THE FIRST FIVE WITH THE OBJECT.SERVER SIDE
-			
-			var found=false;
-			for(var i=0;i<locations.length;++i)
-			{
-				if(this.getAttribute('value')==locations[i].placeId)
-				{
-					found=true;
-					titleInput.value=locations[i].title;
-				}
-			}
-			if(!found)
-			{
-				alert("No matches errror");
-			}
-		});
-	}
-
-}*/
 
