45e00664cdbb9f0ff2ba475a89134e27c19bdad0
cmmccoy1996@gmail.com <cmmccoy2@asu.edu>
2019-01-31 14:30:57 -0700

    t

--- index.js
+++ index.js
@@ -71,8 +71,6 @@ passport.deserializeUser(user.deserializeUser());
 
 app.use(function(req, res, next){
    res.locals.currentUser = req.user;
-   res.locals.success = req.flash('success');
-   res.locals.error = req.flash('error');
    next();
 });
 
